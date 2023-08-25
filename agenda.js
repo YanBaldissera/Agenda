819c2441bdd202467090578993ad8ab21e25e94b
Yan <yanbaldissera1@gmail.com>
2023-08-25 15:50:01 -0300

    first commit

---
+++ agenda.js
@@ -0,0 +1,9 @@
+function novoLembrol() {
+    const novoLembrol = document.getElementById('novoLembrol');
+    novoLembrol.classList.remove('d-none');
+}
+
+function cancelar() {
+    const cancelar = document.getElementById('novoLembrol');
+    cancelar.classList.add('d-none')
+}
